244111c620f28bbebe565d4cb16d4d43f826f2ec
Danilo Lessa Bernardineli <danilo.lessa@gmail.com>
2022-12-17 10:23:04 -0300

    checkpoint

--- scrap.py
+++ scrap.py
@@ -2,50 +2,77 @@
 
 import requests as req
 from bs4 import BeautifulSoup
-from collections import defaultdict
 from tqdm.auto import tqdm
 
 # %%
 
-headers = {
+# Definitions
+
+MAX_THREADS = 30
+
+HEADERS = {
     'User-Agent': 'My User Agent 1.0',
     'From': 'youremail@domain.example'  # This is another valid field
 }
 
-
 URI_CITIES = 'https://deonibus.com/onibus-para'
 SELECTOR_CITIES = ".tab-content > article > article.section-stations > a"
-r = req.get(URI_CITIES, headers=headers)
-soup = BeautifulSoup(r.content, features="html.parser")
-cities_el = soup.select(SELECTOR_CITIES)
 
-
-cities_slugs = {}
-
-for city_el in cities_el:
-    city_uri = city_el.attrs['href']
-    city_slug = city_uri.split("/")[-1]
-    # uf = city_uri.split("-")[-1].upper()
-    name = city_el.text.strip() + f" / {uf}"
-    cities_slugs[name] = city_slug
-# %%
+URI_CITY = "https://deonibus.com/onibus-para/{city_slug}"
 SELECTOR_DEPARTING = "#section-portfolio-routes > div.routes.leaving > a"
 SELECTOR_ARRIVING = "#section-portfolio-routes > div.routes.arriving > a"
-URI_CITY = "https://deonibus.com/onibus-para/{city_slug}"
 
-cities_arrivals = defaultdict(list)
-cities_departures = defaultdict(list)
-for city, city_slug in tqdm(cities_slugs.items()):
-    uri = URI_CITY.format(city_slug=city_slug)
-    r = req.get(uri, headers=headers)
+# %%
+
+# Get all cities
+
+r = req.get(URI_CITIES, headers=HEADERS)
+
+if r.status_code == 200:
     soup = BeautifulSoup(r.content, features="html.parser")
-    arrivals_el = soup.select(SELECTOR_ARRIVING)
-    for el in arrivals_el:
-        slug = el.attrs['href'].split("/")[-1].split("-para-")[0]
-        cities_arrivals[city_slug].append(slug)
+    cities_el = soup.select(SELECTOR_CITIES)
 
-    departures_el = soup.select(SELECTOR_DEPARTING)
-    for el in departures_el:
-        slug = el.attrs['href'].split("/")[-1].split("-para-")[-1]
-        cities_departures[city_slug].append(slug)
+    cities_slugs = [el.attrs['href'].split("/")[-1].strip().lower()
+                    for el
+                    in cities_el]
+else:
+    raise Exception(f"Error: non-code 200 ({r.status_code})")
+# %%
+
+
+def retrieve_city_connections(city_slug: str) -> tuple:
+    city_arrivals = []
+    city_departures = []
+    code = None
+    uri = URI_CITY.format(city_slug=city_slug)
+    r = req.get(uri, headers=HEADERS)
+    if r.status_code == 200:
+        code = 200
+        soup = BeautifulSoup(r.content, features="html.parser")
+        arrivals_el = soup.select(SELECTOR_ARRIVING)
+        for el in arrivals_el:
+            slug = el.attrs['href'].split("/")[-1].split("-para-")[0]
+            city_arrivals.append(slug)
+
+        departures_el = soup.select(SELECTOR_DEPARTING)
+        for el in departures_el:
+            slug = el.attrs['href'].split("/")[-1].split("-para-")[-1]
+            city_departures.append(slug)
+    else:
+        print(f"Non-200 code at {city_slug}")
+        code = r.status_code
+
+    return city_arrivals, city_departures, code
+
+cities_arrivals = {}
+cities_departures = {}
+non_sucessful_slugs = []
+for slug in tqdm(cities_slugs):
+    city_arrivals, city_departures, code = retrieve_city_connections(slug)
+    if code == 200:
+        cities_arrivals[slug] = city_departures
+        cities_departures[slug] = city_departures
+    else:
+        non_sucessful_slugs.append(slug)
+
 # %%
