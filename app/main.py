49a39938c57cfc170d126ec5a5e7d5ae59606c9b
Danilo Lessa Bernardineli <danilo.lessa@gmail.com>
2023-04-24 17:33:12 -0300

    sync

--- app/main.py
+++ app/main.py
@@ -2,8 +2,6 @@ import os
 import streamlit as st
 from script import load_data, plot_map_per_org
 
-df = load_data()
-
 # Define layout
 
 st.set_page_config(
@@ -11,6 +9,8 @@ st.set_page_config(
     page_icon=os.path.join(os.path.dirname(__file__), "assets", "icon.png"),
     layout="wide",
 )
+
+df = load_data()
 
 st.markdown("# Origens e Destinos dos Ônibus Rodoviários do Brasil")
 
